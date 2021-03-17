4b2236505e7b2533ee923f82feef8f16f9c30d87
leejunheee <z6z6jun@naver.com>
2021-03-17 21:53:42 +0900

    3/17

---
+++ JaveDEV/src/CH6/FactorialTest.java
@@ -0,0 +1,22 @@
+package CH6;
+
+public class FactorialTest {
+
+	public static void main(String[] args) {
+		System.out.println(factorial(5)); //FactorialTest.factorial(4)
+		
+	}
+	
+	static long factorial(int n) {
+		long result = 0;
+		
+		if ( n == 1) {
+			result = 1;
+		} else {
+			result = n * factorial(n-1);
+		}
+		
+		return result;
+	}
+
+}
